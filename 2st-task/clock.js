7f00491f3a4507d5eff6d2199211b4b35c835575
Aron Fabian <130830737+AronFabianHu@users.noreply.github.com>
2023-07-01 10:31:33 +0200

    update clock

--- 2st-task/clock.js
+++ 2st-task/clock.js
@@ -1,3 +1,4 @@
+const hand = document.querySelector('.hand')
 const secondHand = document.querySelector('.second-hand')
 const minHand = document.querySelector('.min-hand')
 const hourHand = document.querySelector('.hour-hand')
@@ -10,20 +11,21 @@ const digitSec = document.querySelector('#sec')
 
 
 function setDate() {
+
     const now = new Date()
     const seconds = now.getSeconds();
-    const secondDegrees = ((seconds * 6) + 90)
+    const secondDegrees = ((seconds * 6) - 90)
     secondHand.style.transform = `rotate(${secondDegrees}deg)`
 
 
 
     const minutes = now.getMinutes();
-    const minDegrees = ((minutes * 6) + 90)
+    const minDegrees = ((minutes * 6) - 90)
     minHand.style.transform = `rotate(${minDegrees}deg)`
 
 
     const hours = now.getHours();
-    const hourDegrees = ((hours * 30) + 90)
+    const hourDegrees = ((hours * 30) - 90)
     hourHand.style.transform = `rotate(${hourDegrees}deg)`
     console.log(`${hours}h ${minutes}m ${seconds}s`)
 
@@ -35,6 +37,10 @@ function setDate() {
 }
 
 const s = setInterval(setDate, 1000);
+setDate()
+
+
+
 
 
 
