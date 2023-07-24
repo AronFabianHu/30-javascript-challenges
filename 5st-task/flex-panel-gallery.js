30320af8e03515df8d8446aa0901f13d55380a31
Aron Fabian <130830737+AronFabianHu@users.noreply.github.com>
2023-07-24 22:49:15 +0200

    5th task

---
+++ 5st-task/flex-panel-gallery.js
@@ -0,0 +1,12 @@
+const panels = document.querySelectorAll('.panel');
+
+
+panels.forEach(panel => panel.addEventListener('click', () => {
+    panel.classList.toggle('open')
+}))
+panels.forEach(panel => panel.addEventListener('transitionend', (e) => {
+    if (e.propertyName.includes('flex')) {
+        panel.classList.toggle('open-active')
+    }
+
+}))
